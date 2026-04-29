a1444bfae6267f549843d7dd5ff0cc6715cfb4a8
S-vani <shayan.vanishoja86@gmail.com>
2026-04-29 15:38:11 -0400

    Added some routes to get transactions using filters, delete transactions with their id and return transactions with specific ids. Added helper functions in the services to have cleaner code

--- backend/routes/assets.py
+++ backend/routes/assets.py
@@ -1,28 +1,81 @@
 import uuid
+from datetime import datetime
+from tkinter.tix import Select
 
 from fastapi import HTTPException, File, UploadFile, Form, Depends, APIRouter
 from sqlalchemy import select, Numeric
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 
 from backend.authentication.authentication import current_active_user
 from backend.db.database import get_async_session
 from backend.db_models.assets import Transaction, User
 from backend.schemas.assets import CreateTransaction
 
-from backend.services.asset_services import get_holdings, current_quantity
+from backend.services.asset_services import get_holdings_from_symbol, current_quantity, create_holding_filter, \
+    turn_list_to_dict
 
 router = APIRouter()
 
-@router.get("/holdings/{symbol}")
-async def return_holdings(
-        symbol: str,
+@router.get("/transactions/{trans_id}")
+async def return_holdings_with_id(
+        trans_id: str,
         session: AsyncSession = Depends(get_async_session),
         current_user: User = Depends(current_active_user),
 ):
-    return await get_holdings(session, current_user.id, symbol)
+    result = await session.execute(
+        select(Transaction).where(
+            Transaction.user_id == current_user.id,
+            Transaction.id == trans_id
+        )
+    )
 
-@router.post("/transaction")
+    transaction = result.scalars().first()
+    if transaction is None:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+
+    return transaction
+
+@router.delete("/transactions/{trans_id}")
+async def delete_holding_with_id(
+        trans_id: str,
+        session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(current_active_user),
+):
+    holding = await session.execute(
+        select(Transaction).where(
+            Transaction.user_id == current_user.id,
+            Transaction.id == trans_id
+        )
+    )
+    result = holding.scalars().first()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+
+    await session.delete(result)
+    await session.commit()
+
+    return result
+
+
+@router.get("/transactions")
+async def return_holdings_with_filter(
+        symbol: str | None = None,
+        action: str | None = None,
+        start_date: datetime | None = None,
+        end_date: datetime | None = None,
+        session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(current_active_user),
+):
+    query = create_holding_filter(current_user.id, symbol, action, start_date, end_date)
+
+    result = await session.execute(query.order_by(Transaction.created_at.desc()))
+    result = result.scalars().all()
+
+    return turn_list_to_dict(result)
+
+@router.post("/transactions")
 async def make_transaction(
         data: CreateTransaction,
         session: AsyncSession = Depends(get_async_session),
@@ -33,7 +86,7 @@ async def make_transaction(
         if data.quantity > curr_holdings:
             raise HTTPException(status_code=409, detail="Insufficient holdings") # Error if you are trying to sell more than you have
 
-    purchase = Transaction(
+    transaction = Transaction(
         action=data.action,
         asset_type=data.asset_type,
         symbol=data.symbol,
@@ -41,45 +94,7 @@ async def make_transaction(
         quantity=data.quantity,
         user_id=current_user.id
     )
-    session.add(purchase)
+    session.add(transaction)
     await session.commit()
-    await session.refresh(purchase)
-    return purchase
-
-@router.get("/feed")
-async def get_feed(session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
-    posts = [row[0] for row in result.all()]
-
-    posts_data =[]
-    for post in posts:
-        posts_data.append(
-            {
-                "id": str(post.id),
-                "caption": post.caption,
-                "url": post.url,
-                "file_type": post.file_type,
-                "file_name": post.file_name,
-                "created_at": post.created_at.isoformat()
-            }
-        )
-
-    return {"posts": posts_data}
-
-@router.delete("/posts/{post_id}")
-async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
-    try:
-        post_uuid = uuid.UUID(post_id)
-
-        result = await session.execute(select(Post).where(Post.id == post_uuid))
-        post = result.scalars().first()
-
-        if not post:
-            raise HTTPException(status_code=404, detail="Post not found")
-
-        await session.delete(post)
-        await session.commit()
-
-        return {"success": True, "message": "Post deleted successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    await session.refresh(transaction)
+    return transaction
--- backend/services/asset_services.py
+++ backend/services/asset_services.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.db_models.assets import Transaction
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import UUID
 
-async def get_holdings(db: AsyncSession, user_id:UUID, symbol:str):
+async def get_holdings_from_symbol(db: AsyncSession, user_id:UUID, symbol:str):
     result = await db.execute(
         select(Transaction).where(
             Transaction.user_id == user_id,
@@ -13,8 +15,33 @@ async def get_holdings(db: AsyncSession, user_id:UUID, symbol:str):
 
     return result.scalars().all()
 
+def create_holding_filter(
+                                user_id: UUID,
+                                symbol: str | None = None,
+                                action: str | None = None,
+                                start_date: datetime | None = None,
+                                end_date: datetime | None = None
+                                ):
+    query = select(Transaction).where(Transaction.user_id == user_id)
+
+    if symbol:
+        query = query.where(Transaction.symbol == symbol)
+
+    if action:
+        query = query.where(Transaction.action == action)
+
+    if start_date:
+        query = query.where(Transaction.created_at >= start_date)
+
+    if end_date:  # Will always be true
+        query = query.where(Transaction.created_at <= end_date)
+    else:
+        query = query.where(Transaction.created_at <= datetime.utcnow())
+
+    return query
+
 async def current_quantity(db: AsyncSession, user_id:UUID, symbol:str) -> float:
-    lst = await get_holdings(db, user_id, symbol)
+    lst = await get_holdings_from_symbol(db, user_id, symbol)
     selling = 0
     buying = 0
     for trans in lst:
@@ -24,3 +51,20 @@ async def current_quantity(db: AsyncSession, user_id:UUID, symbol:str) -> float:
             buying += trans.quantity
 
     return buying - selling
+
+def turn_list_to_dict(lst):
+    transactions_data = []
+    for trans in lst:
+        transactions_data.append(
+            {
+                "id": str(trans.id),
+                "user_id": str(trans.user_id),
+                "action": str(trans.action),
+                "asset_type": str(trans.asset_type),
+                "symbol": str(trans.symbol),
+                "price_of_one": float(trans.price_of_one),
+                "quantity": float(trans.quantity),
+                "created_at": trans.created_at.isoformat()
+            }
+        )
+    return transactions_data
