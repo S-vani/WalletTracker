21b3d20f539ca5f0489dcbb908a2a148c59ade93
S-vani <shayan.vanishoja86@gmail.com>
2026-05-19 13:20:37 -0400

    Added css styling for the crypto/stock searching when adding a new transaction

--- frontend/src/Transactions/TransactionSymbolSearch.jsx
+++ frontend/src/Transactions/TransactionSymbolSearch.jsx
@@ -3,10 +3,23 @@ import {searchCrypto, searchStock} from "../services/api.js";
 import TransactionSymbolList from "./TransactionSymbolList.jsx";
 
 function TransactionSymbolSearch({onSub}) {
-    const [type, setType] = useState("stock")
+    const [type, setType] = useState("")
     const [symbol, setSymbol] = useState("");
     const [results, setResults] = useState([]);
     const [showSymbols, setShowSymbols] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const getPlaceholder = () =>{
+        if(type === "stock"){
+            return "AAPL, NVDA, GOOG"
+        }
+        else if (type === "crypto"){
+            return "BTC, SOL, ETH"
+        }
+        else{
+            return "Select a type above"
+        }
+    }
 
 
     const handleChange = (e) => {
@@ -15,6 +28,7 @@ function TransactionSymbolSearch({onSub}) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true)
 
         if (type === "stock") {
             const data = await searchStock(symbol)
@@ -26,20 +40,38 @@ function TransactionSymbolSearch({onSub}) {
             setResults(data)
             setShowSymbols(true)
         }
+        setLoading(false)
 
     };
 
     return (
         <div className="transaction-symbol-wrapper">
-            <h2>Symbol Search</h2>
+            <h2>Search</h2>
+            <div className="search-buttons">
+                <button
+                    className={type === "stock" ? "search-button active" : "search-button"}
+                    onClick={() => setType("stock")}>
+                    Stock
+                </button>
+                <button
+                    className={type === "crypto" ? "search-button active" : "search-button"}
+                    onClick={() => setType("crypto")}>
+                    Crypto
+                </button>
+            </div>
+            <form className="symbol-search-form" onSubmit={handleSubmit}>
+                <input
+                    className="symbol-search-input"
+                    name="symbol"
+                    placeholder={getPlaceholder()}
+                    onChange={handleChange}/>
 
-            <button onClick={() => setType("stock")}>Stock</button>
-            <button onClick={() => setType("crypto")}>Crypto</button>
-            <form onSubmit={handleSubmit}>
-                <input name="symbol" placeholder="Symbol" onChange={handleChange}/>
-
-                <button type="submit">Submit</button>
+                <button className="search-button" type="submit">Search</button>
             </form>
+
+            {loading && (
+                <h3 className="loading-search">Loading...</h3>
+            )}
 
             {showSymbols && (
                 <TransactionSymbolList
